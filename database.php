6580c05b548dd55ccd9b66ab3d28f4bb437560d9
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-08 11:22:04 -0500

    database changed

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'sd686');
-define('USERNAME', 'sd686');
-define('PASSWORD', 'ZOm1EN5l3');
-define('CONNECTION', 'sql1.njit.edu');
+define('DATABASE', 'pra22');
+define('USERNAME', 'pra22');
+define('PASSWORD', 'zsLR8d2wM');
+define('CONNECTION', 'sql2.njit.edu');
