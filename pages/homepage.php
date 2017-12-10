093613c3f90571ce5be25232c86e2476d4169d26
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-10 01:03:11 -0500

    small changes to navbar

--- pages/homepage.php
+++ pages/homepage.php
@@ -17,16 +17,20 @@
 </head>
 
 <body>
-<nav class="navbar navbar-inverse" role="navigation">
-    <div class = "container">
-        <div class= "collapse navbar-collapse" id="collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li> 
-            </ul>
-        </div>
-        
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Final Project</a>
     </div>
-</nav>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+  </nav>
 <h1>
     <?php
 
