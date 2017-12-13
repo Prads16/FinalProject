8b271091fe0d41ad30fc78eda6b34471abf5dea0
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-13 01:34:08 -0500

    minor changes to accounts

--- collections/accounts.php
+++ collections/accounts.php
@@ -6,15 +6,18 @@ class accounts extends \database\collection
 
     //This is the function to write to find user by ID for login.
     //Don't forget to return the object see findOne in the collection class
-    public static function findUserbyEmail($email)
+    public static function findUserbyUsername($username)
     {
             $tableName = get_called_class();
-            $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
+            $sqlquery = 'SELECT * FROM ' . $tableName . ' WHERE email ="' . $username.'"';
          //grab the only record for find one and return as an object
-            $recordsSet = self::getResults($sql, $email);
-            if (is_null($recordsSet)) {
+            $recordsSet = self::getResults($sqlquery);
+            if (is_null($recordsSet)) 
+            {
                 return FALSE;
-            } else {
+            } 
+            else 
+            {
                 return $recordsSet[0];
             }
     }
