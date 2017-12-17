6b869a79c4799a1e59ad4bf61c6cf768839d8927
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-17 13:44:11 -0500

    homepage bootstrap

--- pages/homepage.php
+++ pages/homepage.php
@@ -3,9 +3,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Login Page</title>
+    <meta name="description" content="Login Page">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -15,33 +18,47 @@
     <![endif]-->
 </head>
 
-<body>
-
-
-<h1>
-    <?php
-    //this how to print some data;
-    echo $data['site_name'];
-    ?> </h1>
-
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+<body class="bg-success">
 
 <form action="index.php?page=accounts&action=login" method="POST">
-
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit">Login</button>
+  <nav class="navbar navbar-inverse">
+  <div class="container-fluid"> 
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Web Systems Development-Final Project</a>
     </div>
+  </div>
+  </nav>
+<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+     <h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-offset-4 col-sm-4">
+                <div class="form-group">
+                    <h1 align="center"><a href="#">To-do Application Login</a></h1>
+                    <label for = "Username"><span class="FieldInfo">Username:</span></label>
+                    <input class="form-control" type="text" placeholder="Enter Username" name="email" id="email" required>
 
+                    <label for = "Password"><span class="FieldInfo">Password"</span></label>
+                    <input class="form-control" type="password" placeholder="Enter Password" name="password" id="password" required>
+                    
+                </div>
+                <div class="col-sm-offset-4 col-sm-4">
+            
+            <input class="btn btn-success" type="submit" name="login" value="Login">
+            </div>
+                
+            </div>
+            
+        </div>
+        
 
+        
+    </div>
+    
 </form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
+<div class="form-group col-sm-offset-5">
+                  <label for="no account"><span class="FieldInfo">Don't have a account?</span></label><a href="index.php?page=accounts&action=register"> Register </a>
+</div>
 
 
 <script src="js/scripts.js"></script>
