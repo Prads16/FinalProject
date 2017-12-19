f03c55121bc8fea4ae6625c2d9aa3866854ac4e5
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-19 05:00:17 -0500

    final changes

--- routes/routes.php
+++ routes/routes.php
@@ -31,6 +31,14 @@ class routes
 
         $route = new route();
         $route->http_method = 'POST';
+        $route->action = 'save';
+        $route->page = 'tasks';
+        $route->controller = 'tasksController';
+        $route->method = 'save';
+        $routes[] = $route;
+
+        $route = new route();
+        $route->http_method = 'POST';
         $route->action = 'create';
         $route->page = 'tasks';
         $route->controller = 'tasksController';
