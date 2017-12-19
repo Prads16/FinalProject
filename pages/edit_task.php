236fcc7a4d27dc46fb3011c2fd654cfd834753e8
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-19 04:29:06 -0500

    final changes

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -13,7 +13,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
 		<![endif]-->
 	</head>
-	<body class="bg-success" align="center">
+
 		<body class="bg-success">
 			<nav class="navbar navbar-inverse">
 				<div class="container-fluid">
@@ -37,20 +37,32 @@
 				</div>
 			</nav>
 			<div class = "container" style = "margin-top: 90px;">
-				<center><h1> Edit Task </h1></center>
+				<h1 align="center"><a href="#">Edit Task</a></h1>
 				<div class = "row" style = "margin-top: 30px;">
 				</div><br>
 			</div>
+			<div class="col-sm-offset-4 col-sm-4">
 			<form action="index.php?page=tasks&action=update&id=<?php echo $data->id; ?>" method="post">
-				Created Date : <input type="text" name="createddate" id='createddate' value="<?php echo $data->createddate; ?>"><br>
-				Owner email: : <input type="text" name="owneremail" id='owneremail' value="<?php echo $data->owneremail; ?>"><br>
-				Owner id: <input type="text" name="ownerid" id='ownerid' value="<?php echo $data->ownerid; ?>"><br>
-				Due Date: <input type="text" name="duedate" id='duedate' value="<?php echo $data->duedate; ?>"><br>
-				Message: <input type="text" name="message" id='message' value="<?php echo $data->message; ?>"><br>
-				Isdone: <input type="text" name="isdone" id="isdone" value="<?php echo $data->isdone; ?>"><br>
-				
-				<input type="submit" value="Submit form">
+				<label for ="CreateDate"><span class="FieldInfo">Created Date:</span></label>
+	            <input class="form-control" type="text" name="createddate" id='createddate' value="<?php echo $data->createddate; ?>"><br>
+
+	            <label for ="owneremail"><span class="FieldInfo">Owner email:</span></label>
+	            <input class="form-control" type="text" name="owneremail" id='owneremail' value="<?php echo $data->owneremail; ?>"><br>
+
+	            <label for ="ownerid"><span class="FieldInfo">Owner Id:</span></label>
+	            <input class="form-control" type="text" name="ownerid" id='ownerid' value="<?php echo $data->ownerid; ?>"><br>
+
+	            <label for ="duedate"><span class="FieldInfo">Due Date:</span></label>
+	            <input class="form-control" type="text" name="duedate" id='duedate' value="<?php echo $data->duedate; ?>"><br>
+
+	            <label for ="message"><span class="FieldInfo">Message:</span></label>
+	            <input class="form-control" type="text" name="message" id='message' value="<?php echo $data->message; ?>"><br>
+
+				<input class="btn btn-success" align="center" type="submit" name="save" value="Save changes" >
 			</form>
+			</div>
 			<script src="js/scripts.js"></script>
 		</body>
 	</html>
+
+	
