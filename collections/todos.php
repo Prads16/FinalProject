b0e5438e20b6f8d922e68729668e1d4c70c756bb
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-12-19 04:38:23 -0500

    final changes

--- collections/todos.php
+++ collections/todos.php
@@ -4,7 +4,8 @@ class todos extends database\collection
     protected static $modelName = 'todo';
     //This is the function to write to find tasks by user ID for listing on their homepage.
     //Don't forget to return the record set see findAll in the collection class
-    public static  function findTasksbyID($userid) {
+    public static  function findTasksbyID($userid) 
+    {
         $tableName = get_called_class();
          $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid.'"';
         //echo $sql;
